9e3277cbe10c56097d2f34edd5faa8a9ab9b3c61
Tsviatko Yovtchev <tsviatko@codemerx.com>
2020-06-17 13:02:11 +0300

    Licensing change

--- Mono.Cecil.Shared/Mono.Cecil/AssemblyInfo.cs
+++ Mono.Cecil.Shared/Mono.Cecil/AssemblyInfo.cs
@@ -2,10 +2,11 @@
 // Author:
 //   Jb Evain (jbevain@gmail.com)
 //
+// Copyright (c) 2020 CodeMerx
 // Copyright (c) 2008 - 2015 Jb Evain
 // Copyright (c) 2008 - 2011 Novell, Inc.
 //
-// Licensed under the MIT/X11 license.
+// Licensed under the AGPL
 //
 
 using System.Reflection;

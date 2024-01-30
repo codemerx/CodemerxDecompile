ec9eae45c73a13a70359d0329ae6ed421d7e319b
Alexander Dimitrov <alexander@codemerx.com>
2024-01-31 00:05:25 +0200

    Enable basic decompilation

--- engine/CodemerxDecompile/ViewModels/MainWindowViewModel.cs
+++ engine/CodemerxDecompile/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Mono.Cecil;
 using Telerik.JustDecompiler.Decompiler;
+using Telerik.JustDecompiler.Decompiler.Caching;
+using Telerik.JustDecompiler.Decompiler.WriterContextServices;
+using Telerik.JustDecompiler.Languages;
+using Telerik.JustDecompiler.Languages.CSharp;
 
 namespace CodemerxDecompile.ViewModels;
 
-public class MainWindowViewModel
+public partial class MainWindowViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private Node? selectedNode;
+
+    [ObservableProperty]
+    private string? sourceCode;
+    
     public MainWindowViewModel()
     {
         var assembly = Utilities.GetAssembly(
@@ -15,28 +29,50 @@ public class MainWindowViewModel
         
         Nodes = new ObservableCollection<Node>
         {
-            new Node(assembly.Name.Name, new ObservableCollection<Node>(
-                groupedByNamespace.Select(g => new Node(g.Key == string.Empty ? "<Default namespace>" : g.Key, new ObservableCollection<Node>(
-                    g.Select(t => new Node(t.Name)))))))
+            new()
+            {
+                Title = assembly.Name.Name,
+                SubNodes = new ObservableCollection<Node>(
+                    groupedByNamespace.Select(g => new Node
+                    {
+                        Title = g.Key == string.Empty ? "<Default namespace>" : g.Key,
+                        SubNodes = new ObservableCollection<Node>(
+                            g.Select(t => new Node
+                            {
+                                Title = t.Name,
+                                TypeDefinition = t
+                            })
+                        )
+                    })
+                )
+            }
         };
     }
     
     public ObservableCollection<Node> Nodes { get; }
-    
+
+    partial void OnSelectedNodeChanged(Node? value)
+    {
+        if (value is { TypeDefinition: not null })
+        {
+            var language = LanguageFactory.GetLanguage(CSharpVersion.V7);
+            var stringBuilder = new StringBuilder();
+            var stringWriter = new StringWriter(stringBuilder);
+            var writerSettings = new WriterSettings();
+            var writer = language.GetWriter(new PlainTextFormatter(stringWriter), new SimpleExceptionFormatter(), writerSettings);
+            writer.Write(value.TypeDefinition, new SimpleWriterContextService(new DefaultDecompilationCacheService(), true));
+            SourceCode = stringBuilder.ToString();
+        }
+        else
+        {
+            SourceCode = string.Empty;
+        }
+    }
+
     public class Node
     {
-        public string Title { get; }
-        public ObservableCollection<Node>? SubNodes { get; }
-  
-        public Node(string title)
-        {
-            Title = title;
-        }
-
-        public Node(string title, ObservableCollection<Node> subNodes)
-            : this(title)
-        {
-            SubNodes = subNodes;
-        }
+        public required string Title { get; init; }
+        public ObservableCollection<Node>? SubNodes { get; init; }
+        public TypeDefinition? TypeDefinition { get; init; }
     }
 }

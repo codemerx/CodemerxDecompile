eaf55bb65004314af71063e00bfe19d8bf129086
goojal <amiralimail@gmail.com>
2024-02-02 15:53:18 +0300

    add missing test case files

---
+++ engine/Decompiler.Tests/Integration/Expected/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText("numbers.txt"))
+            {
+                Console.WriteLine($"{reader.ReadToEnd()}");
+            }
+        }
+    }
+}

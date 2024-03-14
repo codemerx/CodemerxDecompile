cba13afc2ef5e7bdbda862422aa2cb0db426495c
Alexander Dimitrov <alexander@codemerx.com>
2024-03-14 21:43:27 +0200

    Remove strong signing and InternalsVisibleTo with public keys

--- src/JustDecompileCmd.Shared/AssemblyInfo.cs
+++ src/JustDecompileCmd.Shared/AssemblyInfo.cs
@@ -31,11 +31,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 /* AGPL */
-#if RELEASE
-[assembly: InternalsVisibleTo("ConsoleRunner, PublicKey=00240000048000009400000006020000002400005253413100040000010001008d3ec49d2c78b3f6029c19200a28c4a8dc279a9dfddcc7413c0737c73b49e08a3761e148e745fe2007d8d057a962c7eaf22e7eded052bb08f1e7f0d7794db4827b09124ffa61625879af57120f8078fad84c8c7d4f6c6ebb9ab14de089d606ca0ed66b9af0c67795fa4e34f61ce62732180d06fb67b3ec93b202f045e10a99d3")]
-[assembly: InternalsVisibleTo("ConsoleRunnerDotNet6, PublicKey=00240000048000009400000006020000002400005253413100040000010001008d3ec49d2c78b3f6029c19200a28c4a8dc279a9dfddcc7413c0737c73b49e08a3761e148e745fe2007d8d057a962c7eaf22e7eded052bb08f1e7f0d7794db4827b09124ffa61625879af57120f8078fad84c8c7d4f6c6ebb9ab14de089d606ca0ed66b9af0c67795fa4e34f61ce62732180d06fb67b3ec93b202f045e10a99d3")]
-#else
 [assembly: InternalsVisibleTo("ConsoleRunner")]
 [assembly: InternalsVisibleTo("ConsoleRunnerDotNet6")]
 /* End AGPL */
-#endif

2bcce974eb2aac17ccef87d14a61cb538dc0127b
Alexander Dimitrov <alexander@codemerx.com>
2024-03-14 20:39:40 +0200

    Update workflows and build script to use src folder

--- build/macos/build-application-bundle.sh
+++ build/macos/build-application-bundle.sh
@@ -6,4 +6,4 @@ script_dir=$(dirname $0)
 cp $script_dir/Info.plist CodemerxDecompile.app/Contents
 cp $script_dir/codemerx-logo.icns CodemerxDecompile.app/Contents/Resources
 
-cp -Rp engine/CodemerxDecompile/bin/Release/net7.0/osx-arm64/publish/ CodemerxDecompile.app/Contents/MacOS
+cp -Rp src/CodemerxDecompile/bin/Release/net7.0/osx-arm64/publish/ CodemerxDecompile.app/Contents/MacOS

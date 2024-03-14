58fc8ffd45f4540a67333c2dbf391cd7eb73e5c9
Alexander Dimitrov <alexander@codemerx.com>
2024-03-14 20:13:29 +0200

    Remove VS Code. Remove old packaging workflow.

--- ui/src/vs/workbench/contrib/files/electron-sandbox/fileActions.contribution.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-/* AGPL */
-import 'vs/workbench/contrib/files/browser/fileActions.contribution';
-/* End AGPL */

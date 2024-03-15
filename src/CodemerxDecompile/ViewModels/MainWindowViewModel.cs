60529fc51cfcea144f486117d507f43e4c06e350
Alexander Dimitrov <alexander@codemerx.com>
2024-03-15 16:55:04 +0200

    Add license text to all files in CodemerxDecompile project

--- src/CodemerxDecompile/ViewModels/MainWindowViewModel.cs
+++ src/CodemerxDecompile/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+    Copyright CodeMerx 2024
+    This file is part of CodemerxDecompile.
+
+    CodemerxDecompile is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CodemerxDecompile is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with CodemerxDecompile.  If not, see<https://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;

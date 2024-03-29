42be4fdbecd87ebfeaccf98b794c0e42b0acf1b4
Alexander Dimitrov <alexander@codemerx.com>
2024-03-29 18:40:56 +0200

    Update version to 1.4.0

--- src/CodemerxDecompile.Shared/CommonVersionAssemblyInfo.cs
+++ src/CodemerxDecompile.Shared/CommonVersionAssemblyInfo.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
